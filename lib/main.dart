7b4b54f6090541d4103df4c1ff167e2c1dc4c817
NimraAbid05 <nimram214@gmail.com>
2023-01-21 12:48:55 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:contact_app/views/home.dart';
+import 'package:flutter/material.dart';
 
-class MyHome extends StatefulWidget {
-  const MyHome({super.key});
-
-  @override
-  State<MyHome> createState() => _MyHomeState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _MyHomeState extends State<MyHome> {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Home_View(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,11 +1,71 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class Home_View extends StatefulWidget {
+  const Home_View({super.key});
 
   @override
+  State<Home_View> createState() => _Home_ViewState();
+}
+
+class _Home_ViewState extends State<Home_View> {
+  List contact = [
+    {"name": "Asma", "number": "03000067009"},
+    {"name": "Ali", "number": "03003456789"},
+    {"name": "Babar", "number": "03232323232"},
+    {"name": "Javad", "number": "03131313131"},
+    {"name": "Kiran", "number": "03009090909"},
+    {"name": "Lubna", "number": "03003456789"},
+    {"name": "Maha", "number": "03003456789"},
+    {"name": "Nimra", "number": "03003456789"},
+    {"name": "Noor", "number": "03003456789"},
+    {"name": "Noreen", "number": "03003456789"},
+    {"name": "Orangzeb", "number": "03003456789"},
+    {"name": "Osman", "number": "03003456789"},
+    {"name": "Pari", "number": "03003456789"},
+    {"name": "Sana", "number": "03003456789"},
+    {"name": "Sadaf", "number": "03003456789"},
+    {"name": "Saad", "number": "03003456789"},
+    {"name": "Zubair", "number": "03003456789"},
+    {"name": "Zuha", "number": "03003456789"},
+  ];
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.black,
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
+        ],
+        title: Text(
+          "Contacts",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: ListView.builder(
+          itemCount: contact.length,
+          itemBuilder: (context, i) {
+            return InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("${contact[i]['name']}"),
+                subtitle: Text("${contact[i]['number']}"),
+                leading: CircleAvatar(
+                  backgroundColor: Colors.black,
+                  radius: 20,
+                  child: Icon(Icons.person,),
+                ),
+              ),
+            );
+          }),
+    );
   }
 }
